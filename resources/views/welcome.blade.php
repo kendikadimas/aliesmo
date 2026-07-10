f12947c166daaf3b725be12c8b634a09050bbe4b
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2026-07-10 15:10:45 +0700

    fix: force light mode via Tailwind custom-variant and inline script

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <!-- Force light mode before any rendering -->
+    <script>document.documentElement.classList.remove('dark');document.documentElement.classList.add('light');</script>
     @vite(['src/main.js'])
 </head>
 <body>
